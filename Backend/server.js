78643722431541b8fe5c1ef794ce9e26e4d22a03
kvenkat9889 <kvenkat9889@gmail.com>
2025-06-24 16:56:10 +0530

    Updated code

--- Backend/server.js
+++ Backend/server.js
@@ -4,14 +4,14 @@ const cors = require('cors');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = 3340;
 
 // PostgreSQL database connection
 const pool = new Pool({
     user: 'postgres', 
-    host: 'localhost',
+    host: 'postgres',
     database: 'payslip_db', 
-    password: 'root', 
+    password: 'admin123', 
     port: 5432,
 });
 
@@ -114,5 +114,5 @@ app.get('/api/payslips/:empCode', async (req, res) => {
 
 // Start the server
 app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
+    console.log(`Server running on http://3.85.61.23:${port}`);
 });
